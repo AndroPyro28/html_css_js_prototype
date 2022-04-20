9c2855f5b9d346799bccb729a5316611ae38dc35
AndroPyro28 <Menandroeugenio1028@gmail.com>
2022-04-20 22:08:09 +0800

    first commit

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+const carouselWrapper = document.querySelector('.carousel__wrapper');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const leftButton = document.querySelector('.left');
+const rightButton = document.querySelector('.right');
+let currentCarouselContent = 0;
+
+    leftButton.addEventListener('click', () => {
+        if( 0 < currentCarouselContent) {
+        carouselWrapper.style.transform = `translateX(${--currentCarouselContent * -100}vw)`;
+        // clearInterval(sliderFunc);
+        }
+    })
+    rightButton.addEventListener('click', () => {
+        if(currentCarouselContent < carouselItems.length -1) {
+        carouselWrapper.style.transform = `translateX(${++currentCarouselContent * -100}vw)`;
+        // clearInterval(sliderFunc);
+        }
+    })
+
+    
+    // const sliderFunc = () => {
+    //     if(currentCarouselContent == carouselItems.length -1) return currentCarouselContent = -1;
+    //     carouselWrapper.style.transform = `translateX(${++currentCarouselContent * -100}vw)`;
+    // }
+
+    // setInterval(sliderFunc, 4000)
+    
+
